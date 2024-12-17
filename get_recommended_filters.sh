c713239cc57012c41fd6de01dfd51845dbb5a00c
Rosaani <hayneswalsh338213@gmail.com>
2024-12-18 00:13:27 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#!/bin/bash
 
-node download_lists.js blocklist
+# declare an array of urls
+urls=(
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
+  https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+  https://adaway.org/hosts.txt
+  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
@@ -10,4 +18,9 @@ node download_lists.js blocklist
   https://v.firebog.net/hosts/Easyprivacy.txt
   https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_19_Annoyances_Popups/filter.txt
   https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt
-  
+)
+# download all files in parallel and append them to blocklist.txt
+node download_lists.js blocklist.txt ${urls[@]}
+# print a message when done
+echo "Done. The blocklist.txt file contains merged data from recommended filter lists."
+node download_lists.js blocklist
