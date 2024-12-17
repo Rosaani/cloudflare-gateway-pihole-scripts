d3e1c07c05f4e9688417d0488e9f9db0c1fead8a
Rosaani <hayneswalsh338213@gmail.com>
2024-12-17 21:09:30 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -6,3 +6,8 @@ node download_lists.js blocklist
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.plus.txt
+  https://v.firebog.net/hosts/Easylist.txt
+  https://v.firebog.net/hosts/Easyprivacy.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_19_Annoyances_Popups/filter.txt
+  https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt
+  
